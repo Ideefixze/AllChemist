1659737da9a8c51ddd8ef1e8cf0eb2011ee24661
Ideefixze <kaczooorqu@gmail.com>
2020-10-12 21:59:50 +0200

    Dodaj pliki projektów.

---
+++ AllChemist/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AllChemist
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly string Version = "0.0.1";
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            //TODO: Something for better logging etc.
+            System.Console.WriteLine($"Starting AllChemist {Version}");
+        }
+    }
+}
---
+++ AllChemist/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AllChemist
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "AllChemist "+App.Version+"v";
+            CreateWorldGrid();
+        }
+
+        public void CreateWorldGrid(int sizeX = 600, int sizeY = 600, int columns = 20, int rows = 20)
+        {
+            Grid worldGrid = new Grid();
+            worldGrid.Width = sizeX;
+            worldGrid.Height = sizeY;
+            worldGrid.HorizontalAlignment = HorizontalAlignment.Left;
+            worldGrid.VerticalAlignment = VerticalAlignment.Center;
+            worldGrid.ShowGridLines = true;
+
+
+            for (int i = 0; i < columns; i++)
+            {
+                ColumnDefinition cd = new ColumnDefinition();
+                cd.MaxWidth = sizeX / columns;
+                worldGrid.ColumnDefinitions.Add(cd);
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                RowDefinition rd = new RowDefinition();
+                rd.MaxHeight = sizeY / rows;
+                worldGrid.RowDefinitions.Add(rd);
+            }
+
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Grid g = new Grid();
+                    g.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    g.VerticalAlignment = VerticalAlignment.Center;
+                    
+
+                    Emoji.Wpf.TextBlock textBlock = new Emoji.Wpf.TextBlock();
+                    textBlock.Text = "◼";
+                    textBlock.FontSize = 16;
+                    textBlock.TextAlignment = TextAlignment.Center;
+                    textBlock.MaxHeight = sizeY / rows;
+                    textBlock.MaxWidth = sizeX / columns;
+
+                    g.Children.Add(textBlock);
+
+                    Grid.SetRow(g, j);
+                    Grid.SetColumn(g, i);
+                    worldGrid.Children.Add(g);
+                }
+            }
+
+            MainGrid.Children.Add(worldGrid);
+        }
+    }
+}
